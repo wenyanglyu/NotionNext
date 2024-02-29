51812d3255f3df90496116d6568d82f8820b8fb2
wenyang <121678379+wenyanglyu@users.noreply.github.com>
2024-02-29 23:06:10 +0200

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,7 +13,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Kia Ora，I am wenyang,Kia Ora，I am a network engineer,Kia Ora，I am half a coder, welcome to my blog',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Kia Ora，I am wenyang,Kia Ora，I am a network engineer,Kia Ora，I am an AUT Master, Kia Ora，I am also a writer,Kia Ora，I am also kind of a coder,Kia Ora，also a little game designer, welcome to my blog',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
